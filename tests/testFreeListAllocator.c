f51a38bd774b5bfba4fd6be517da254e9dbb2b08
adimemedovski <adimemedovski.dev@gmail.com>
2026-07-11 00:56:20 +1000

    Rewrite begun.

--- tests/testFreeListAllocator.c
+++ tests/testFreeListAllocator.c
@@ -11,25 +11,9 @@ void tearDown(void) {
 
 }
 
-void testInitFreeList(void) {
-    FreeList freeList;
-    
-    /*
-     * Test One: Testing basic functionality.
-     */
-    TEST_ASSERT_TRUE(initFreeList(&freeList));
-    
-    /*
-     * Cleanup.
-     */
-    munmap(freeList.basePtr, MEMORY_REGION_SIZE);
-}
-
-
 int main(void) {
     UNITY_BEGIN();
     
-    RUN_TEST(testInitFreeList);
 
     return UNITY_END();
 }
