6b32a5b4dba0a002bd36d869fae7b10943cef9dd
adimemedovski <adimemedovski.dev@gmail.com>
2026-07-17 07:49:34 +1000

    Having a lot of trouble with handling the cases of freeListAlloc.

--- include/freeListAllocator.h
+++ include/freeListAllocator.h
@@ -5,5 +5,39 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#define MAX_MEMORY_SIZE 4096
+
+/*
+ * When freeing memory, the total block size will be:
+ *     allocatedMemory + sizeof(metaData) + totalPadding
+ *
+ * How do we determine what totalPadding is? Well, we need to take 
+ * into account three things:
+ *     1. The alignmentPadding of the pointer;
+ *     2. the alignmentPadding of the meta data;
+ *     3. the alignmentPadding of the block header.
+ */
+
+typedef struct {
+    size_t allocatedMemory;
+    size_t alignmentPaddingOfPtr;
+    size_t alignmentPaddingOfMetaData;
+} MetaData;
+
+typedef struct Block {
+    struct Block *nextBlock;
+    size_t blockSize;
+} Block;
+
+typedef struct {
+    void *memoryBasePtr; 
+    Block *head; 
+} FreeList;
+
+bool initFreeList(FreeList *freeList);
+
+void *freeListAlloc(FreeList *freeList, size_t blockSize, size_t alignment);
+
+bool freeAlloc(FreeList *freeList, void *ptr);
 
 #endif
--- tests/testFreeListAllocator.c
+++ tests/testFreeListAllocator.c
@@ -11,24 +11,47 @@ void tearDown(void) {
 
 }
 
-void testInitMemoryManager(void) {
-    MemoryManager memoryManager;
+void testInitFreeList(void) {
+    FreeList freeList;
 
-    TEST_ASSERT_TRUE(initMemoryManager(&memoryManager));
-    TEST_ASSERT_TRUE(memoryManager.ptrToVirtualAddressSpace != NULL); 
-    TEST_ASSERT_TRUE(memoryManager.ptrToVirtualAddressSpace != MAP_FAILED); 
+    TEST_ASSERT_TRUE(initFreeList(&freeList));
     
-    /*
-     * Clean up.
-     */
-    munmap(memoryManager.ptrToVirtualAddressSpace, MEMORY_REGION_SIZE);
+    TEST_ASSERT_TRUE(freeList.memoryBasePtr != NULL);
+    TEST_ASSERT_TRUE(freeList.memoryBasePtr != MAP_FAILED);
+    
+    munmap(freeList.memoryBasePtr, MAX_MEMORY_SIZE);
 }
 
+void testFreeListAlloc(void) {
+    FreeList freeList;
+    initFreeList(&freeList);
+
+    size_t *ptrOne = (size_t*) freeListAlloc(&freeList, sizeof(size_t) * 2, _Alignof(size_t));
+    
+    TEST_ASSERT_TRUE(ptrOne != NULL);
+    ptrOne[0] = 3;
+    ptrOne[1] = 2;
+    ptrOne[2] = 1;
+
+    TEST_ASSERT_EQUAL_size_t(3, ptrOne[0]);
+    TEST_ASSERT_EQUAL_size_t(2, ptrOne[1]);
+    TEST_ASSERT_EQUAL_size_t(1, ptrOne[2]);
+    
+    size_t *ptrTwo = (size_t*) freeListAlloc(&freeList, sizeof(size_t) * 2, _Alignof(size_t));
+    ptrTwo[0] = 666;
+
+    TEST_ASSERT_EQUAL_size_t(3, ptrOne[0]);
+    TEST_ASSERT_EQUAL_size_t(2, ptrOne[1]);
+    TEST_ASSERT_EQUAL_size_t(1, ptrOne[2]);
+    
+    TEST_ASSERT_EQUAL_size_t(1, ptrTwo[0]);
+}
 
 int main(void) {
     UNITY_BEGIN();
 
-    RUN_TEST(testInitMemoryManager);
+    RUN_TEST(testInitFreeList);
+    RUN_TEST(testFreeListAlloc);
 
     return UNITY_END();
 }
