a79ea5e32c4fe4a77bed53279c0bcb84417c2414
adimemedovski <adimemedovski.dev@gmail.com>
2026-07-21 06:39:12 +1000

    Added some tests for freeListAlloc.

--- tests/testFreeListAllocator.c
+++ tests/testFreeListAllocator.c
@@ -41,14 +41,45 @@ void testFreeListAlloc(void) {
     /*
      * Testing first allocation functionality.
      */
+    size_t *ptrOne = (size_t*) freeListAlloc(&freeList, sizeof(size_t) * 3, _Alignof(size_t));        
     
 
+    /*
+     * Testing if ptrOne is aligned in case where no padding is needed.
+     */
+    TEST_ASSERT_TRUE((size_t) ptrOne % _Alignof(size_t) == 0);
+  
+    /*
+     * Testing if metaData is aligned.
+     */
+    char *retrieveMetaData = (char*) ptrOne;
+    retrieveMetaData -= sizeof(MetaData);
+    
+    TEST_ASSERT_TRUE((size_t) retrieveMetaData % _Alignof(MetaData) == 0);
+   
+    /*
+     * Testing if new block is ahead of ptr by blockSize and 
+     * if it is aligned.
+     */
+    char *retrieveNewBlock = (char*) ptrOne;
+    retrieveNewBlock += sizeof(size_t) * 3;
+    
+    TEST_ASSERT_TRUE(freeList.head == (Block*) retrieveNewBlock);
+    TEST_ASSERT_TRUE((size_t) retrieveNewBlock % _Alignof(Block) == 0); 
+    
+
+    char* ptrTwo = (char*) freeListAlloc(&freeList, sizeof(char), _Alignof(char));
+    TEST_ASSERT_TRUE((size_t) freeList.head % _Alignof(Block) == 0);  
+
+    size_t *ptrThree = (size_t*) freeListAlloc(&freeList, sizeof(size_t), _Alignof(size_t));
+
+    TEST_ASSERT_TRUE((size_t) ptrThree % _Alignof(size_t) == 0);
 }
 
 int main(void) {
     UNITY_BEGIN();
 
     RUN_TEST(testInitFreeList);
-
+    RUN_TEST(testFreeListAlloc);
     return UNITY_END();
 }
